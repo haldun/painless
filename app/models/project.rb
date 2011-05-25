3f743f14b83d4c8b2cd76a53944ae2385111cdf0
Haldun Bayhantopcu <hb@haldun.me>
2011-05-25 05:38:57 +0300

    Small fixes

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ActiveRecord::Base
   belongs_to :user
   has_many :features, :dependent => :destroy
-  has_many :tasks, :dependent => :destroy
+  has_many :tasks, :dependent => :destroy, :order => "created_at desc"
 
   def to_s
     name
